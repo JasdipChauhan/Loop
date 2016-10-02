5878a0ca3fa7f9acacdd4e55c8b0f8d15b1906b3
JasdipC <jasdip.chauhan@gmail.com>
2016-10-01 21:14:17 -0400

    initial commit

---
+++ LoopAndroid/app/src/main/java/com/gfive/jasdipc/loopandroid/RidesActivity.java
@@ -0,0 +1,13 @@
+package com.gfive.jasdipc.loopandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RidesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rides);
+    }
+}

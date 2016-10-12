65ea86b019c1e1368ce9a9c61bfe430398eed9fb
JasdipC <jasdip.chauhan@gmail.com>
2016-10-11 20:23:02 -0400

    using generic class based views

--- LoopServer/rides/views.py
+++ LoopServer/rides/views.py
@@ -8,52 +8,23 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.views import APIView
 from django.http import Http404
+from rest_framework import mixins
+
 
 
 from .models import *
 from .serializers import *
 
-class RideList(APIView):
+class RideList(generics.ListCreateAPIView):
     """
     List all rides, or create a new ride
     """
-    def get(self, request, format=None):
-        rides = Ride.objects.all()
-        rideSerializer = RideSerializer(rides, many=True)
-        return Response(rideSerializer.data)
+    queryset = Ride.objects.all()
+    serializer_class = RideSerializer
 
-    def post(self, request, format=None):
-        rideSerializer = RideSerializer(data=request.data)
-        if rideSerializer.is_valid():
-            rideSerializer.save()
-            return Response(rideSerializer.data, status=status.HTTP_201_CREATED)
-        return Response(rideSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class RideDetail(APIView):
+class RideDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a ride instance.
     """
-    def get_object(self, pk):
-        try:
-            return Ride.objects.get(pk=pk)
-        except Ride.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        ride = self.get_object(pk)
-        serializer = RideSerializer(ride)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        ride = self.get_object(pk)
-        serializer = RideSerializer(ride, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        ride = self.get_object(pk)
-        ride.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    queryset = Ride.objects.all()
+    serializer_class = RideSerializer

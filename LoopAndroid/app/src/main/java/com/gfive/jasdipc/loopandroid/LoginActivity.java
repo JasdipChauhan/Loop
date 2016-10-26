f7cd593ee5a2c9c6d632cb0b9dd54c678149e061
JasdipC <jasdip.chauhan@gmail.com>
2016-10-25 21:41:39 -0400

    facebook login works

---
+++ LoopAndroid/app/src/main/java/com/gfive/jasdipc/loopandroid/LoginActivity.java
@@ -0,0 +1,58 @@
+package com.gfive.jasdipc.loopandroid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private LoginButton loginButton;
+    private TextView loginTV;
+    private CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(getApplication());
+        callbackManager = CallbackManager.Factory.create();
+
+        setContentView(R.layout.activity_login);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginTV = (TextView) findViewById(R.id.login_TV);
+        loginButton.setReadPermissions("public_profile");
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                loginTV.setText(loginResult.getAccessToken().getUserId());
+            }
+
+            @Override
+            public void onCancel() {
+                loginTV.setText("User cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                loginTV.setText("Error logging in");
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+}
